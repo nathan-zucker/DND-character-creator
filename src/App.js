f763f252921aa0a450a909d3dbd9975ad082ccc4
nathan-zucker <114187880+nathan-zucker@users.noreply.github.com>
2022-12-23 11:36:22 -0500

    PROGRESS ON BASE STATS AND RACE SELECTION

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='Header'>
+        <Navbar />
       </header>
+      <body>
+        <h1 className="Title">Hey, this is Nathan!</h1>
+        <MainContent />
+        <Button />
+      </body>
     </div>
   );
 }
+
+function Navbar() {
+  return (
+    <div>
+      <h1>I am empty</h1>
+    </div>
+  );
+}
+
+function MainContent() {
+  const itemOne = 'hello!';
+  const itemTwo = 'hi!';
+  const itemThree = 'goodbye!';
+  return (
+    <div className="Main">
+      <p className='Description'>Here's a whole bunch of content for ya.</p>
+      <ul className='List'>
+        <li className='ListItem'>{itemOne}</li>
+        <li className='ListItem'>{itemTwo}</li>
+        <li className='ListItem'>{itemThree}</li>
+      </ul>
+    </div>
+  );
+}
+
+class Button extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      name: 'Nathan',
+      age: 23,
+      location: 'West Virginia',
+      hobby: 'guitar'
+    }
+  }
+  render() {
+    return(
+      <div>
+        <button className='button'>Click Me!</button>
+      </div>
+    );
+  }
+}
+
 
 export default App;
